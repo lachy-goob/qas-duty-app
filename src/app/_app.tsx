8eab32a97a7a4294820c478ca48e21c304e30e11
Lachlan <lachlangoebel@gmail.com>
2025-04-20 10:02:06 +1000

    Updated Clerk Key + Security Updates

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -3,7 +3,10 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
