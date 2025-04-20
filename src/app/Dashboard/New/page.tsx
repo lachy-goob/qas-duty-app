1856c15b27152ad40659e0819a674266f6484d2f
Lachlan <lachlangoebel@gmail.com>
2025-04-20 12:11:14 +1000

    Fix #5?

--- src/app/Dashboard/New/page.tsx
+++ src/app/Dashboard/New/page.tsx
@@ -3,8 +3,9 @@ import JobFormController from "./JobFormController";
 
 const JobFormPage = async () => {
   const user = await currentUser();
+  const userData = user ? { id: user.id } : null; // Extract only the `id` field
 
-  return <JobFormController user={user} />;
+  return <JobFormController user={userData} />;
 };
 
 export default JobFormPage;
